8be0d3017839b55e47c629ddfe9581b3ff998741
Santiago Rui <117700156+SantiagoRuiDev@users.noreply.github.com>
2024-01-25 10:20:09 -0300

    Version 25/1 - Fix

--- src/socket.js
+++ src/socket.js
@@ -9,4 +9,4 @@ export const state = reactive({
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = (Tools.STATUS) ? Tools.API : Tools.DEV;
 
-export const socket = io(URL)
+export const socket = io(URL, {origins:"*"})
